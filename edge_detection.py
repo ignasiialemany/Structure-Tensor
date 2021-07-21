c8a2d1e159f2296aba8ab4d4f74c8068157eefa5
ignasiialemany <59746390+ignasiialemany@users.noreply.github.com>
2021-07-21 17:03:54 +0100

    Modified file name

--- edge_detection.py
+++ edge_detection.py
@@ -2,10 +2,6 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-#img = mpimg.imread('mask.jpg')
-#imgplot = plt.imshow(img)
-
-
 def plot_img(img):
     return plt.imshow(img)
 
