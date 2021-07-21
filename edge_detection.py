eead6aa35902bd2a169bbeee07dc306c0367ddb4
ignasiialemany <59746390+ignasiialemany@users.noreply.github.com>
2021-07-21 18:04:32 +0100

    Added structure tensor computations

--- edge_detection.py
+++ edge_detection.py
@@ -21,15 +21,43 @@ def computeConvolution(img,kernel):
     return convoluted_matrix
 
 def edgeDetection(img):
-    #Based on Sobel
+    #Based on Sobel matrices
     edge_dx = computeConvolution(img,np.array([[1 ,0 ,-1],[2, 0 ,-2],[1, 0 ,-1]]))
     edge_dy = computeConvolution(img,np.array([[-1 ,-2, -1],[0, 0, 0],[1, 2 ,1]]))
     return edge_dx, edge_dy
+
+def compute_structure_tensor(partial_x,partial_y):
+    dx2 = np.multiply(partial_x, partial_x)
+    dy2 = np.multiply(partial_y, partial_y)
+    dxdy = np.multiply(partial_x, partial_y)
+    size_x,size_y = dx2.shape
+    structure_tensor = np.zeros([size_x,size_y,2,2])
+    eigenvectors = np.zeros([size_x,size_y,2,2])
+    eigenvalues = np.zeros([size_x,size_y,2])
+    for i in range (0,size_x):
+        for j in range (0,size_y):
+            structure_tensor[i, j, 0, 0] = dx2[i,j]
+            structure_tensor[i, j, 0, 0] = dy2[i,j]
+            structure_tensor[i, j, 0, 1], structure_tensor[i, j, 1, 0] = dxdy[i,j], dxdy[i,j]
+            eigValues, eigVectors = np.linalg.eig(structure_tensor[i,j,:,:])
+            eigenvectors[i,j,:,0] = eigVectors[:,0]
+            eigenvectors[i,j,:,1] = eigVectors[:,1]
+            eigenvalues[i,j,0] = eigValues[0]
+            eigenvalues[i,j,1] = eigValues[1]
+    return structure_tensor, eigenvectors, eigenvalues
+
 
 if __name__ == "__main__":
 
     img = load_img("mask.jpg")
     partial_x, partial_y = edgeDetection(img)
+    structure_tensor, eigenvectors, eigenvalues = compute_structure_tensor(partial_x,partial_y)
+    np.save("structure_tensor.npy",structure_tensor)
+    np.save("eigenvectors.npy",eigenvectors)
+    np.save("eigenvalues.npy",eigenvalues)
+
+
+
 
 
 
